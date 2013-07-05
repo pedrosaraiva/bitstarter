c1e8e4e931d32cdff88e415493291433b789a8e9
psaraiva <pedrosaraiva1@gmail.com>
2013-07-05 20:20:45 +0000

    Change files

--- web.js
+++ web.js
@@ -1,12 +1,26 @@
 var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express.createServer(express.logger()), index;;
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ response.send(index.toString());
+});
+
+
+function readIndex(callback) {
+  fs.readFile("./index.html", function (err, content) {
+	if (err) return callback(err);
+	callback(content);
+    });
+}
+
+readIndex( function (content) {
+    index = new Buffer(content,'utf-8');
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });

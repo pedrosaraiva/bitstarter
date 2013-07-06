059639f7bbffbe07f6ff4af14140676c29a4041d
psaraiva <pedrosaraiva1@gmail.com>
2013-07-06 18:19:22 +0000

    Add node_modules cheerio, commander and resteler. Change grader.js to accept url

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,39 +37,54 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var loadChecks = function(checksFile) {
+    return JSON.parse(fs.readFileSync(checksFile));
 };
 
-var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
-};
+var checkHtmlFile = function(htmlFile, url, checksFile) {
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-	var present = $(checks[ii]).length > 0;
-	out[checks[ii]] = present;
+    if(url !== undefined){
+	rest.get(url).on('complete', function(data){ parseHtmlFile(data, checksFile); });
+    } else {
+	parseHtmlFile(fs.readFileSync(htmlFile), checksFile);
     }
-    return out;
+
+
 };
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
+
 };
+
+
+function parseHtmlFile(file, checksfile){
+
+    $ = cheerio.load(file);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+
+    var outJson = JSON.stringify(out, null, 4);
+
+    console.log(outJson);
+}
 
 if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>, "Url to index.html"')
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    checkHtmlFile(program.file, program.url, program.checks);
+
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
